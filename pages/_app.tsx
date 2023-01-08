e79af2f4f1a0d9e8a157b6b900350d3c3343c8a6
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-08 09:44:33 +0530

    feat: add font configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
+import { Montserrat } from '@next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-montserrat',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${montserrat.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
