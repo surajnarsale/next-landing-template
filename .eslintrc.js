8d50effe61bba3eff070f714f683f38372496796
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-08 23:36:26 +0530

    feat: dummy type safe component

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
@@ -17,4 +17,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
+  // rules: {
+  //   '@typescript-eslint/restrict-template-expressions': 'warn',
+  // },
 }
