e0e41f30481f67132c3a3908ad78e612289b4ef6
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-08 09:14:55 +0530

    chore: add prettier class formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className=" hover:bg-green-400 text-center bg-blue-400 ">HomePage</h1>
+      <h1 className="bg-blue-400  text-center hover:bg-green-400 ">HomePage</h1>
     </>
   )
 }
