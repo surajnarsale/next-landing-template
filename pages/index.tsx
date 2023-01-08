c1bd515d894e497aee4820c02f74ead6bb704005
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-08 09:55:18 +0530

    chore: add framer-motion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import { BeakerIcon } from '@heroicons/react/24/solid'
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
     <>
       <BeakerIcon className="h-6 w-6 text-blue-500" />
-      <h1 className="bg-blue-400 text-center  text-9xl hover:bg-green-400 ">HomePage</h1>
+      <motion.h1
+        animate={{ y: 100 }}
+        className="bg-blue-400 text-center  text-9xl hover:bg-green-400 "
+      >
+        HomePage
+      </motion.h1>
     </>
   )
 }
