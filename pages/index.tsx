cd31083ddc6892fe6767f97d2987426fba901118
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-08 09:47:25 +0530

    feat: add heroicons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import { BeakerIcon } from '@heroicons/react/24/solid'
+
 export default function Home() {
   return (
     <>
+      <BeakerIcon className="h-6 w-6 text-blue-500" />
       <h1 className="bg-blue-400 text-center  text-9xl hover:bg-green-400 ">HomePage</h1>
     </>
   )
