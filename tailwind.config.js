9b734838e542d337e3f2a2434725e9582cda7d92
Suraj Narsale <70274294+surajnarsale@users.noreply.github.com>
2023-01-10 21:52:16 +0530

    fix: remove custom tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,39 +6,42 @@ module.exports = {
       sans: ['var(--font-montserrat)'],
       // other fonts families goes here
     },
-    fontSize: {
-      xs: ['14px', { lineHeight: '24px', letterSpacing: '-0.03em' }],
-      sm: ['16px', { lineHeight: '28px', letterSpacing: '-0.03em' }],
-      lg: ['18px', { lineHeight: '28px', letterSpacing: '-0.03em' }],
-      x1: ['24px', { lineHeight: '36px', letterSpacing: '-0.03em' }],
 
-      '2xl': ['36px', { lineHeight: '48px', letterSpacing: '-0.032em' }],
-      '3xl': ['48px', { lineHeight: '56px', letterSpacing: '-0.032em' }],
-      '4xl': ['56px', { lineHeight: '64px', letterSpacing: '-0.032em' }],
-      '5xl': ['80px', { lineHeight: '80px', letterSpacing: '-0.032em' }],
-    },
-    colors: {
-      white: '#FFFFFF',
-      purple: '#3F3CBB',
-      midnight: '#121063',
-      metal: '#565584',
-
-      'tahiti-blue': '#3AB7BF',
-      'cool-white': '#ECEBFF',
-      'bubble-gum': '#FF77E9',
-      'copper-rust': '#78DCCA',
-    },
-    screens: {
-      tablet: '960px',
-      desktop: '1248px',
-    },
     extend: {},
   },
   plugins: [],
 }
 
-// font family file
-// font sizes
-// colors
-// breakpoints
-//
+// theme: {
+//   fontFamily: {
+//     sans: ['var(--font-montserrat)'],
+//     // other fonts families goes here
+//   },
+//   fontSize: {
+//     xs: ['14px', { lineHeight: '24px', letterSpacing: '-0.03em' }],
+//     sm: ['16px', { lineHeight: '28px', letterSpacing: '-0.03em' }],
+//     lg: ['18px', { lineHeight: '28px', letterSpacing: '-0.03em' }],
+//     x1: ['24px', { lineHeight: '36px', letterSpacing: '-0.03em' }],
+
+//     '2xl': ['36px', { lineHeight: '48px', letterSpacing: '-0.032em' }],
+//     '3xl': ['48px', { lineHeight: '56px', letterSpacing: '-0.032em' }],
+//     '4xl': ['56px', { lineHeight: '64px', letterSpacing: '-0.032em' }],
+//     '5xl': ['80px', { lineHeight: '80px', letterSpacing: '-0.032em' }],
+//   },
+//   colors: {
+//     white: '#FFFFFF',
+//     purple: '#3F3CBB',
+//     midnight: '#121063',
+//     metal: '#565584',
+
+//     'tahiti-blue': '#3AB7BF',
+//     'cool-white': '#ECEBFF',
+//     'bubble-gum': '#FF77E9',
+//     'copper-rust': '#78DCCA',
+//   },
+//   screens: {
+//     tablet: '960px',
+//     desktop: '1248px',
+//   },
+//   extend: {},
+// },
